c0a3fc191dfd9e20ddc6d6ecb9caf3236e2ee4b0
Juan Ignacio Sosa <juanisosa@gmail.com>
2019-03-01 16:56:22 -0500

    Added function to get input on course numbers

--- stv.py
+++ stv.py
@@ -1,8 +1,26 @@
-VOTE_DATA = { 'americas': 'americas.csv'}
+import time
+import pandas as pd
 
-def main():
+def get_parameters(course_count):
+    #Ask the user for the number of courses to select using while loop to ensure a valid number is entered. Gets df index lenght as input
+    while True:
+        try:
+            course_no = int(input('\nPlease enter the number of courses to select. The maximum number of courses is {}: '.format(course_count)))
+            if course_no <= course_count:
+                break
+        except KeyboardInterrupt: #To allow terminating the program during input
+            print('\n')
+            break
+        except:
+            print('\nThat\'s not a valid city number! Select a number between 1 and {}: '.format(course_count))
+
+    print('-'*40)
+    return course_no
+
+#We load the survey data into a DataFrame
+voting_data = pd.read_csv('americas.csv')
+course_count = len(voting_data.index) #determine the number of courses offered in survey
+course_no = get_parameters(course_count) #get courses to select from user
 
 
-
-if __name__ == "__main__":
-	main()
+print('There are {} courses and you chose to pick {}.'.format(course_count, course_no)) #temp as a test of load
