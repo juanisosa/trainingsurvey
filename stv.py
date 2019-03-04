e7ffed6c16697c44f0d1907b67cb6790a374e422
juanisosa <juanisosa@gmail.com>
2019-03-03 20:40:16 -0500

    Playing with dataframe and index

--- stv.py
+++ stv.py
@@ -1,8 +1,10 @@
-import time
 import pandas as pd
+import numpy as np
 
-def get_parameters(course_count):
-    #Ask the user for the number of courses to select using while loop to ensure a valid number is entered. Gets df index lenght as input
+def get_parameters(course_count): #get user input
+    """Ask the user for the number of courses to select using while
+    loop to ensure a valid number is entered. Gets df index lenght
+    as input"""
     while True:
         try:
             course_no = int(input('\nPlease enter the number of courses to select. The maximum number of courses is {}: '.format(course_count)))
@@ -12,15 +14,16 @@ def get_parameters(course_count):
             print('\n')
             break
         except:
-            print('\nThat\'s not a valid city number! Select a number between 1 and {}: '.format(course_count))
+            print('\nThat\'s not a valid city number! Select a number between 1 and {}: \n'.format(course_count))
 
     print('-'*40)
     return course_no
 
 #We load the survey data into a DataFrame
-voting_data = pd.read_csv('americas.csv')
+voting_data = pd.read_csv('americas.csv', index_col='course')
 course_count = len(voting_data.index) #determine the number of courses offered in survey
 course_no = get_parameters(course_count) #get courses to select from user
 
-
-print('There are {} courses and you chose to pick {}.'.format(course_count, course_no)) #temp as a test of load
+print('\nThere are {} courses and you chose to pick {}.\n\n'.format(course_count, course_no)) #temp as a test of load
+print('-'*40)
+print(voting_data['index'])
